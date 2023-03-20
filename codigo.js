4ea34f24d12f95d758c8e1a5be76cbde991165b0
aleobre <alejanmago@gmail.com>
2023-03-19 20:03:47 -0600

    Tarea 1

---
+++ codigo.js
@@ -0,0 +1,10 @@
+//Crear evento de click bton
+let btn_presionar = document.querySelector(".presionar");
+    btn_presionar.addEventListener('click',function(){
+
+    let etiqueta = document.querySelector('#contenedor')
+    `<p>
+   
+    </p>`;
+});
+})
